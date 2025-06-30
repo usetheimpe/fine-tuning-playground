de47799de9e668eaaa915c2afdb08efef00d4731
usetheimpe <yelim27@student.ubc.ca>
2025-06-30 01:00:09 -0700

    test

--- test_agent.py
+++ test_agent.py
@@ -1,8 +1,8 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 
-os.environ['GOOGLE_API_KEY'] = 'AIzaSyAy12JNzOWFlWcQoTvlhVtUlklUGnh4lZw'
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+api_key_system = os.getenv("GOOGLE_API_KEY")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", api_key=api_key_system)
 prompt="Which government department is Elon Must Heading currently?"
 print ("The prompt is:", prompt)
 llm_output=llm.invoke(prompt)
